bd5ed1f95d3d42357dd35617291f6061d3b369ce
NJ <226465585+nj-twice@users.noreply.github.com>
2026-04-09 22:22:26 +0200

    Rewrite filename sequence check

--- src/main.rs
+++ src/main.rs
@@ -7,166 +7,6 @@ const FIRST_FRAME: &str = "1_1.png";
 const MARGIN: u32 = 2;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    match args.iter().count() - 1 {
-        0 => panic!("No argument supplied!"),
-        1 => {}
-        _ => panic!("More than one argument supplied!"),
-    }
-
-    let action = &args[1];
-
-    match action.as_str() {
-        "make" => make_spritesheet(),
-        "play" => visualize_animation(),
-        "help" => show_help(),
-        // TODO: add an edit subcommand: deconstructs a sprite sheet into properly named files, wait for edits, then rebuild the sprite sheet
-        _ => panic!("Invalid action!"),
-    }
-}
-
-fn show_help() {
-    print!(
-        "
-pixanim - Tool to help you make pixel art animations
-
-USAGE: pixanim <ACTION>
-
-ACTIONs:
-    help     Show this help message.
-    make     Build the spritesheet from the image files in the current
-             directory.
-    play     Build the spritesheet and start playing the resulting
-             animation in a new window.
-"
-    );
-}
-
-
-fn check_paths(image_paths: &Vec<String>) {
-    // Here, we don't care that the files are actually PNGs or not.
-    // We just enforce the naming convetion.
-
-    // Check that the row no.1 exists
-    let one_counts = image_paths
-        .iter()
-        .filter(|x| x.as_str().starts_with("1_"))
-        .count();
-
-    if one_counts == 0 {
-        panic!("Error: row no.1 wasn't found!")
-    }
-
-    // Check that no number skips happened:
-    // Parse frame numbers into a sorted vector of uints
-    // Compare length of vec and largest frame number
-    // If they differ, error.
-
-    let mut row_no: u32 = 1;
-
-    let mut ones: Vec<_> = image_paths.iter()
-        .filter(|x| x.as_str().starts_with(format!("{row_no}_").as_str()))
-        .map(|x| x.split_once('_').unwrap().1)
-        .map(|x| x.split_once('.').unwrap().0.parse::<u32>().unwrap())
-        .collect();
-    ones.sort();
-    ones.reverse();
-
-    let largest = *ones.get(0).unwrap();
-    let length = ones.len() as u32;
-    if length != largest {
-        println!("At row no.{row_no}");
-        println!("Largest frame index: {largest} != Length: {length}");
-        panic!("Number skip happened!")
-    }
-
-    #[cfg(debug_assertions)]
-    println!("Found {one_counts} instances of row no.{row_no}");
-
-    // Determine number of columns for all rows from first row
-
-    let max_columns: u32 = length;
-
-    // Repeat what was done for row no.1 but for subsequent rows
-    loop {
-        row_no += 1;
-
-        // Check for existence
-
-        let num_count = image_paths
-            .iter()
-            .filter(|x| x.as_str().starts_with(format!("{row_no}_").as_str()))
-            .count();
-
-        if num_count == 0 {
-
-            // This happens both for when a row was skipped
-            // and when it is the last row.
-            // We need to check if any other path beyond those checked
-            // previously still exists.
-            // If at least one does, there's a skip. Error.
-            // Else, the row from the last iteration was the last,
-            // we break out of the loop.
-
-            let remaining_paths = image_paths
-                .iter()
-                // Skip the first checked elements from the past rows
-                .skip( (row_no as usize - 1) * (max_columns as usize) )
-                .count();
-
-            #[cfg(debug_assertions)]
-            println!("Number of remaining unchecked paths: {remaining_paths}");
-
-            if remaining_paths == 0  {
-
-                #[cfg(debug_assertions)]
-                println!("No unchecked path remaining. Continuing.");
-
-                break
-            }
-
-            #[cfg(debug_assertions)]
-            println!("Paths remaining, not last item. Error!");
-
-            panic!("{}", format!("Error: row no.{row_no} wasn't found!"))
-        }
-
-        #[cfg(debug_assertions)]
-        println!("Found {num_count} instances of row no.{row_no}");
-
-        // Check that no skips happened
-
-        let mut columns: Vec<_> = image_paths.iter()
-            .filter(|x| x.as_str().starts_with(format!("{row_no}_").as_str()))
-            .map(|x| x.split_once('_').unwrap().1)
-            .map(|x| x.split_once('.').unwrap().0.parse::<u32>().unwrap())
-            .collect();
-        columns.sort();
-        columns.reverse();
-
-        let largest = *columns.get(0).unwrap();
-        let length = columns.len() as u32;
-        if length != largest {
-            println!("At row no.{row_no}");
-            println!("Largest frame index: {largest} != Length: {length}");
-            panic!("Number skip happened!")
-        }
-
-        #[cfg(debug_assertions)]
-        println!("No skips happened for row no.{row_no}");
-
-        // Check that column count for the current row matches that of row no.1
-
-        if max_columns != length {
-            println!("Number of columns for row no.{row_no} doesn't match first row.");
-            panic!("Column count mismatch!");
-        }
-
-        #[cfg(debug_assertions)]
-        println!("Number of columns of row no.{row_no} matches that of 1st row.");
-
-    }
 }
 
 struct Point {
@@ -175,8 +15,6 @@ struct Point {
 }
 
 fn make_spritesheet() {
-    let paths = get_images_paths();
-    check_paths(&paths);
     check_image_dimensions(&paths);
 
     // Actually build the spritesheet
@@ -270,31 +108,6 @@ fn check_image_dimensions(paths: &Vec<String>) {
     }
 }
 
-fn get_images_paths() -> Vec<String> {
-    let paths = fs::read_dir(".").unwrap();
-    let re = Regex::new("^[0-9]+_[0-9]+.png$").unwrap();
-
-    let filtered_paths: Vec<_> = paths
-        .filter(|x| re.is_match(x.as_ref().unwrap().file_name().to_str().unwrap()))
-        .map(|x| x.unwrap())
-        .collect();
-
-    #[cfg(debug_assertions)]
-    {
-        println!("Printing filtered paths");
-        for path in &filtered_paths {
-            println!("{}", path.file_name().display());
-        }
-        println!("------\n");
-    }
-
-    // Make it a Vec of owned Strings to make work easier
-    let path_strings: Vec<_> = filtered_paths.iter()
-        .map(|x| x.file_name().to_str().unwrap().to_string())
-        .collect();
-
-    path_strings
-}
 
 // TODO: use macroquad to play animation
 //       - interactively changeg FPS
--- src2/main.py
+++ src2/main.py
@@ -1,11 +1,16 @@
 import argparse
 from PIL import Image as img
+import os
+import re
+
+pattern = re.compile(r"^[0-9]+_[0-9]+.png$")
 
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title="actions")
     parser_make = subparsers.add_parser("make", help = "Build the sprite sheet from the image files in the current directory")
     parser_play = subparsers.add_parser("play", help = "Build the sprite sheet and start playing the resulting animation in a new window")
+
     parser_make.set_defaults(func=make)
     parser_play.set_defaults(func=play)
     args = parser.parse_args()
@@ -13,6 +18,76 @@ def main():
 
 def make():
     print("Making the sprite sheet!")
+    paths: list[str] = get_image_paths()
+    print(paths)
+    check_paths(paths)
+
+def check_paths(img_paths: list[str]):
+    row_number = 1
+
+    # Only for declaration. A value will be put later.
+    max_columns = None
+
+    while True:
+        # Check that the row exists
+        col_count = 0
+        for path in img_paths:
+            if path.startswith(f"{row_number}_"):
+                col_count += 1
+        if col_count == 0:
+            # This happens both if a row was skipped or if it was the last one.
+
+            err_msg = f"Row number {row_number} wasn't found!"
+
+            if row_number == 1:
+                # Unconditional error if we're dealing with the first row
+                raise RuntimeError(err_msg)
+            else:
+                # If we're in a row >=2, then we got a max columns count value
+                # we can check against to differentiate cases where row is skipped
+                # and those where last row.
+
+                remaining_paths = len(img_paths) - ((row_number - 1) * max_columns)
+                # If some unchecked paths remain, it means we skipped a row
+                if remaining_paths == 0:
+                    print("No unchecked paths left. Continuing with build.")
+                    break
+                else:
+                    raise RuntimeError(err_msg)
+
+
+        # Check that row exists and no number skips happened:
+        # For each row, parse frame numbers into a sorted list of integers.
+        # Compare length and largest frame number.
+        # If they differ, error.
+        frames_in_row = [
+            int(frame.split("_")[1].split(".")[0])
+                for frame in img_paths if frame.startswith(f"{row_number}")
+        ]
+        frames_in_row.sort()
+        frames_in_row.reverse()
+        largest = frames_in_row[0]
+        length = len(frames_in_row)
+        if length != largest:
+            print(f"At row no.{row_number}")
+            print(f"Largest frame index: {largest} != Length: {length}")
+            raise RuntimeError("Number skip happened!")
+
+        # Only according to the first row, determine total number of columns
+        if row_number == 1:
+            max_columns = length
+        else:
+            if max_columns != length:
+                raise RuntimeError(
+                    f"Column count mismatch with 1st row for row number {row_number}"
+                )
+
+def get_image_paths() -> list[str]:
+    paths: list[str] = []
+    for filename in os.listdir("."):
+        if os.path.isfile(filename) and pattern.match(filename):
+            paths.append(filename)
+    return paths
 
 def play():
     make()
