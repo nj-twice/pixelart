29da5804d40dea9feef535ba7cdc930d1ec98845
NJ <226465585+nj-twice@users.noreply.github.com>
2026-04-14 19:57:45 +0200

    Set proper identity and delete old code

--- viewer/main.lua
+++ viewer/main.lua
@@ -1,23 +1,8 @@
 function love.load()
-  love.filesystem.setIdentity("nj_pixanim")
+  love.filesystem.setIdentity("github.nj-twice.pixelart")
   SAVE_DIR = love.filesystem.getSaveDirectory()
   print(SAVE_DIR)
-
-  EXPECTED_FILENAME = "spritesheet.png"
-
-  if not love.filesystem.exists(EXPECTED_FILENAME) then
-    love.filesystem.createDirectory("placeholder")
-    print("DIR_NOT_FOUND = " .. SAVE_DIR)
-    print("FILE_NOT_FOUND = " .. SAVE_DIR .. "/" .. EXPECTED_FILENAME)
-    love.event.quit(1)
-  end
-
-  local metadata_file = "metadata.txt"
-  METADATA = love.filesystem.read(metadata_file)
-  print(METADATA)
-  TILE = {}
-  --    Width,   Height,  Margin
-  _, _, TILE[1], TILE[2], TILE[3] = string.find(METADATA, "(%d+),(%d+),(%d+)")
+  love.filesystem.createDirectory("placeholder")
 end
 
 function love.update(dt)
@@ -25,12 +10,6 @@ end
 
 
 function love.draw()
-  local image = love.graphics.newImage(EXPECTED_FILENAME)
-  love.graphics.draw(image)
-  for i, v in ipairs(TILE) do
-    love.graphics.print(v, 100, 100+20*i)
-  end
-
 end
 
 function love.keypress(key)
